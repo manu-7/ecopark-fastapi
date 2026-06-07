9362e4d16423182a2d9b42bc0323a2ae4aad0adc
manu-7 <129076796+manu-7@users.noreply.github.com>
2026-06-07 07:40:06 +0530

    fix: add vercel URL to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://ecopark-fastapi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
